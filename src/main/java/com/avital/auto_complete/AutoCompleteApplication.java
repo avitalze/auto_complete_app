11b93f815c7ba4d4eddda27460b7804dbebebbe2
avital <avitalz57@gmail.com>
2021-06-17 18:15:56 +0300

    add add parameter to get url . it work!

--- src/main/java/com/avital/auto_complete/AutoCompleteApplication.java
+++ src/main/java/com/avital/auto_complete/AutoCompleteApplication.java
@@ -3,11 +3,19 @@ package com.avital.auto_complete;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.InputStream;
+
 @SpringBootApplication
 public class AutoCompleteApplication {
 
 	public static void main(String[] args) {
 		// Load the provided list of names to an H2 database
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classloader.getResourceAsStream("BoyNames.txt");
+
+//		URL url = Resources.getResource("BoyNames.txt");
+//		String text = Resources.toString(url, StandardCharsets.UTF_8);
+
 
 		SpringApplication.run(AutoCompleteApplication.class, args);
 	}
--- src/main/java/com/avital/auto_complete/AutoCompleteController.java
+++ src/main/java/com/avital/auto_complete/AutoCompleteController.java
@@ -4,6 +4,7 @@ package com.avital.auto_complete;
 import com.avital.auto_complete.logic.Trie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,8 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/AutoComplete")
 public class AutoCompleteController {
-    List <String> tempList=new ArrayList<>();
-    private Trie paEmulatorEngine=new Trie(tempList);
+    List <String> tempList=List.of("Jacob","Joshua","Joseph","Daniel","David", "D", "William");//new ArrayList<>();
+    private Trie trie=new Trie(tempList);
 
 
     // ----- for check
@@ -24,9 +25,19 @@ public class AutoCompleteController {
     }
 
     @GetMapping("/getOptions")
-    public List<String> getCoralStatus() {
+    public List<String> getCoralStatus(@RequestParam String prefix) {
+        // URL : http://localhost:9090/AutoComplete/getOptions?prefix=J
+        // it case sensative !
+        System.out.println(prefix);
+        List<String> ans_fromUrl= trie.suggest(prefix);
 
-        return List.of("hi","my","friend");//new ArrayList<>();
+        List<String> ans1= trie.suggest("j");
+        List<String> ans2= trie.suggest("J");
+        List<String> ans3= trie.suggest("Jo");
+        List<String> ans4= trie.suggest("D");
+        List<String> ans5= trie.suggest("W");
+
+        return ans_fromUrl; //List.of("hi","my","friend");
     }
 
 
